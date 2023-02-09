cfec0b4c2a0e5c971236e5ccdeb57791218db533
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-02-09 21:14:16 +0100

    chore: add admin role check hook

--- src/components/organisms/users-table-tool/users-table-tool.organism.tsx
+++ src/components/organisms/users-table-tool/users-table-tool.organism.tsx
@@ -1,14 +1,12 @@
 import { ChangeEvent, useCallback, useContext } from 'react'
-import { useReactiveVar } from '@apollo/client'
 import { Button } from '@mui/material'
 import { TableSearchContext } from '../../templates/table/table.context'
-import { authService } from '../../../graphql/auth/auth.service'
-import { UserRole } from '../../../constants/user-role.constants'
 import { SearchInput } from '../../molecules/search-input'
+import { useAdminRole } from '../../../hooks/use-admin-role.hook'
 
 export const UsersTableTool = () => {
   const { search, setSearch } = useContext(TableSearchContext)
-  const user = useReactiveVar(authService.user$)
+  const isAdmin = useAdminRole()
 
   const handleSearch = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value)
@@ -17,7 +15,7 @@ export const UsersTableTool = () => {
   return (
     <>
       <SearchInput value={search} onChange={handleSearch} />
-      {user?.role === UserRole.Admin && <Button variant="outlined">Create Employee</Button>}
+      {isAdmin && <Button variant="outlined">Create Employee</Button>}
     </>
   )
 }
---
+++ src/hooks/use-admin-role.hook.ts
@@ -0,0 +1,8 @@
+import { useReactiveVar } from '@apollo/client'
+import { authService } from '../graphql/auth/auth.service'
+import { UserRole } from '../constants/user-role.constants'
+
+export const useAdminRole = () => {
+  const user = useReactiveVar(authService.user$)
+  return user?.role === UserRole.Admin
+}
