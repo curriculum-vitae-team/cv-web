3ed1326b197da92dc91c99de16429aabf7a1b962
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-02-09 21:17:31 +0100

    refactor: bind search input to context

--- src/components/organisms/languages-table-tool/languages-table-tool.tsx
+++ src/components/organisms/languages-table-tool/languages-table-tool.tsx
@@ -1,20 +1,13 @@
-import { ChangeEvent, useCallback, useContext } from 'react'
 import { Button } from '@mui/material'
 import { SearchInput } from '../../molecules/search-input'
-import { TableSearchContext } from '../../templates/table/table.context'
 import { useAdminRole } from '../../../hooks/use-admin-role.hook'
 
 export const LanguagesTableTool = () => {
-  const { search, setSearch } = useContext(TableSearchContext)
   const isAdmin = useAdminRole()
-
-  const handleSearch = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value)
-  }, [])
 
   return (
     <>
-      <SearchInput value={search} onChange={handleSearch} />
+      <SearchInput />
       {isAdmin && <Button variant="outlined">Create Language</Button>}
     </>
   )
