7fa842fbd53796a4a2d81886d4c939cc01a58320
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-02-09 21:41:59 +0100

    chore: add empty lines

--- src/hooks/use-avatar.hook.ts
+++ src/hooks/use-avatar.hook.ts
@@ -6,12 +6,15 @@ import { USER } from '../graphql/users'
 
 export const useAvatar = (): [MutationFunction<UploadAvatarResult>, MutationFunction, boolean] => {
   const { id } = useParams()
+
   const [uploadAvatar, { loading: uploading }] = useMutation<UploadAvatarResult>(UPLOAD_AVATAR, {
     refetchQueries: [{ query: USER, variables: { id } }]
   })
+
   const [deleteAvatar, { loading: deleting }] = useMutation(DELETE_AVATAR, {
     refetchQueries: [{ query: USER, variables: { id } }]
   })
+
   const loading = uploading || deleting
   return [uploadAvatar, deleteAvatar, loading]
 }
