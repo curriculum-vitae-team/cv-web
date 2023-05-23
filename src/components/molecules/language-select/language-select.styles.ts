7eb535981a56fda037b5568ff856e9dcb61c33a1
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-05-23 13:13:49 +0300

    feat: add language select

---
+++ src/components/molecules/language-select/language-select.styles.ts
@@ -0,0 +1,17 @@
+import { styled, TextField } from '@mui/material'
+
+export const Select = styled(TextField)({
+  marginRight: 48,
+  '& .MuiSelect-select': {
+    boxShadow: 'none !important',
+    background: 'transparent',
+    color: 'white',
+    paddingLeft: 12
+  },
+  '& .MuiSelect-icon': {
+    fill: 'white'
+  },
+  '& .MuiOutlinedInput-notchedOutline': {
+    border: 'none'
+  }
+})
--- src/components/organisms/header/header.organism.tsx
+++ src/components/organisms/header/header.organism.tsx
@@ -1,4 +1,5 @@
 import { Container } from '@mui/material'
+import { LanguageSelect } from '@molecules/language-select'
 import { SideMenu } from '../side-menu'
 import { UserMenu } from '../user-menu'
 import * as Styled from './header.styles'
@@ -11,6 +12,7 @@ export const Header = () => {
           <SideMenu />
         </Styled.LeftPart>
         <Styled.RightPart>
+          <LanguageSelect />
           <UserMenu />
         </Styled.RightPart>
       </Container>
