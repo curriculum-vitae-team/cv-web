f80548973ddc7f932c0fc0e66cc9b34fa45da6bb
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2023-10-15 12:08:18 +0200

    style: projects table

--- src/components/organisms/projects-table-head/projects-table-head.organism.tsx
+++ src/components/organisms/projects-table-head/projects-table-head.organism.tsx
@@ -1,13 +1,14 @@
 import { useTranslation } from 'react-i18next'
-import { TableCell, TableRow } from '@mui/material'
+import { TableCell } from '@mui/material'
 import { createSortLabel } from '@atoms/sort-label'
 import { IProject } from 'interfaces/project.interface'
+import * as Styled from './projects-table-head.styles'
 
 export const ProjectsTableHead = () => {
   const { t } = useTranslation()
 
   return (
-    <TableRow>
+    <Styled.Row>
       <TableCell>{createSortLabel<IProject>('name', t('Name'))}</TableCell>
       <TableCell>{createSortLabel<IProject>('internal_name', t('Internal Name'))}</TableCell>
       <TableCell>{createSortLabel<IProject>('domain', t('Domain'))}</TableCell>
@@ -15,6 +16,6 @@ export const ProjectsTableHead = () => {
       <TableCell>{createSortLabel<IProject>('end_date', t('End Date'))}</TableCell>
       <TableCell>{createSortLabel<IProject>('team_size', t('Team Size'))}</TableCell>
       <TableCell />
-    </TableRow>
+    </Styled.Row>
   )
 }
---
+++ src/components/organisms/projects-table-row/projects-table-row.styles.ts
@@ -0,0 +1,1 @@
+export { Row } from '../projects-table-head/projects-table-head.styles'
