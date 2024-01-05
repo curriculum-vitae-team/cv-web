406ad68b192707f02d1cf9a2585d7aff03711937
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2024-01-05 12:37:21 +0100

    style: user skills layout

--- src/components/pages/employee-skills/employee-skills.styles.ts
+++ src/components/pages/employee-skills/employee-skills.styles.ts
@@ -1,7 +1,14 @@
 import { styled } from '@mui/material'
 
-export const Page = styled('div')({
+export const Page = styled('div')(({ theme }) => ({
   display: 'grid',
   gridTemplateColumns: '1fr 1fr 1fr',
-  gap: 32
-})
+  gap: 32,
+  [theme.breakpoints.down('md')]: {
+    gridTemplateColumns: '1fr 1fr',
+    padding: '0 32px'
+  },
+  [theme.breakpoints.down('sm')]: {
+    gridTemplateColumns: '1fr'
+  }
+}))
