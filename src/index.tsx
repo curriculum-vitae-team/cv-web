12b800628c1708882eaf76369f7f63b1b384a891
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2024-07-15 12:36:32 +0200

    ci: update apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './sentry'
 import './i18n'
-import { injectSpeedInsights } from '@vercel/speed-insights'
 import { App } from './components/app'
-
-injectSpeedInsights({ debug: false })
 
 const element = document.getElementById('root') as HTMLElement
 const root = createRoot(element)
