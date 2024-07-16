253f1c72c3cd12e3b8324e5735446c3e2d12ac77
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2024-07-17 01:40:22 +0200

    fix: disable sentry for localhost

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -7,21 +7,19 @@ import {
 } from 'react-router-dom'
 import { init, reactRouterV6BrowserTracingIntegration, replayIntegration } from '@sentry/react'
 
-if (process.env.NODE_ENV === 'production') {
-  init({
-    dsn: process.env.SENTRY_DNS_URL,
-    integrations: [
-      reactRouterV6BrowserTracingIntegration({
-        useEffect,
-        useLocation,
-        useNavigationType,
-        createRoutesFromChildren,
-        matchRoutes
-      }),
-      replayIntegration()
-    ],
-    tracesSampleRate: 1.0,
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0
-  })
-}
+init({
+  dsn: process.env.SENTRY_DNS_URL,
+  integrations: [
+    reactRouterV6BrowserTracingIntegration({
+      useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes
+    }),
+    replayIntegration()
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
+})
